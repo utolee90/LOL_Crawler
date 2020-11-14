6d4cbb20f433aab937290c7622816c7fb1e34333
utolee90 <itdatabasewiki@gmail.com>
2020-11-14 21:09:17 +0900

    main.py - 문자열 제어 함수를 이용해 다양한 값을 일원화하기

--- Crawl/main.py
+++ Crawl/main.py
@@ -13,7 +13,7 @@ CORS(app)
 #chromedriver option - headless option
 options = webdriver.ChromeOptions() #headless option  사용 - 창 띄우지 않고 실행
 options.add_argument('headless') #반드시 지정
-options.add_argument('window-size=1920x1080') #size에 맞추어서 디자인. 사실 불필요. 
+#options.add_argument('window-size=1920x1080') #size에 맞추어서 디자인. 사실 불필요. 
 #options.add_argument("disable-gpu") #gpu 가속 끄기.
 
 #user data 생성
@@ -36,18 +36,28 @@ with open('champion_id_and_name_list.json', 'r', encoding='utf8') as f:
 with open('champion_id_and_name_list_kor.json', 'r', encoding='utf8') as g:
     id_name_kor_list = json.load(g)
 
+#champ name to lowercase - without space.
+name_eng_simple = []
+for val  in id_name_list.values():
+    name_eng_simple.append(val.lower().replace('\'', '').replace(' ', ''))
+name_kor_simple = []
+for val in id_name_kor_list.values():
+    name_kor_simple.append(val.replace(' ', ''))
+    
+
 def match_kor_eng(name): #이름 확인
-    global id_name_list, id_name_kor_list
+    global id_name_list, id_name_kor_list, name_eng_simple, name_kor_simple
     key='0'
-    if name in id_name_list.values():
+    name = name.lower().replace('\'', '').replace(' ', '').replace('_', '').replace('+','') #단순화
+    if name in name_eng_simple: #영어 이름이면...
         for i in id_name_list.keys():
-            if id_name_list[i] == name:
-                key = str(i)
+            if name_eng_simple[i] == name:
+                key = str(i) #키값 찾기.
                 break
         return id_name_kor_list[key]
-    elif name in id_name_kor_list.values():
+    elif name in name_kor_simple:
         for i in id_name_kor_list.keys():
-            if id_name_kor_list[i] == name:
+            if name_kor_simple[i] == name:
                 key = str(i)
                 break
         return id_name_list[key]
@@ -92,25 +102,27 @@ def user(name):
 
 @app.route('/champ/<name>')
 def champ(name):
-    global champ_data, id_name_list, id_name_kor_list, options
+    global champ_data, id_name_list, id_name_kor_list, name_eng_simple, name_kor_simple, options
     nowtime = float(time.time())
+    if str(type(name)) == "<type 'str'>":
+        name = name.lower().replace('\'', '').replace('_', '').replace('+','') #simplify name
     if not champ_data.get(name) or nowtime - champ_data[name]['time']>86400: #데이터가 없거나 하루 이상 지날 때 추가
         champ_data_new = {}
-        if name.title() in id_name_list.values():
-            p_name = name.lower() #소문자로 바꾸어주기... 
-        elif name in id_name_list.keys(): 
-            p_name = id_name_list[name].lower()
-        elif name in id_name_kor_list.values():
-            p_name = match_kor_eng(name).lower()
+        if name in name_eng_simple: #english
+            p_name = name
+        elif name in id_name_list.keys():  #number
+            p_name = name_eng_simple[name]
+        elif name in name_kor_simple: #korean
+            p_name = match_kor_eng(name)
         driver= webdriver.Chrome() #headless가 오작동... 할수 없이 head 켜고 작동
         driver.get(f'https://www.leagueofgraphs.com/champions/stats/{p_name}/kr')
         time.sleep(10)
-        total_popularity = driver.find_element_by_xpath("//*[@id=\"graphDD1\"]").text.strip()
-        total_win_rate = driver.find_element_by_xpath("//*[@id=\"graphDD2\"]").text.strip()
-        total_ban_rate = driver.find_element_by_xpath("//*[@id=\"graphDD3\"]").text.strip()
+        total_popularity = driver.find_element_by_id("graphDD1").get_attribute('innerText')
+        total_win_rate = driver.find_element_by_id("graphDD2").get_attribute('innerText')
+        total_ban_rate = driver.find_element_by_id("graphDD3").get_attribute('innerText')
         champ_data_new[p_name] = {'popularity':total_popularity, 'win': total_win_rate, 'ban': total_ban_rate }
         
-        trs = driver.find_elements_by_xpath("//*[@id=\"mainContent\"]/div[2]/div[2]/div[1]/table/tbody/tr")
+        trs = driver.find_elements_by_xpath("/html/body/div[2]/div[3]/div[3]/div[2]/div[2]/div[2]/div[2]/div[1]/table/tbody/tr")
         for tr in trs[1:]:
             try:
                 pos = tr.find_element_by_xpath('./td[1]/a').text.strip()
@@ -124,13 +136,14 @@ def champ(name):
         champ_data_new['time'] = float(time.time())
         if len(champ_data_new.keys())>1: #챔피언이 하나라도 등록될 때에만.
             champ_data[name] = champ_data_new
-        driver.quit() #드라이버를 끄자..
+        
 
     
     champ_data_str = json.dumps(champ_data[name])
     with open('champ.json', 'w', encoding='utf8') as f:
         json.dump(champ_data, f)
     
+    driver.quit() #드라이버를 끄자..
     return champ_data_str
 
 
