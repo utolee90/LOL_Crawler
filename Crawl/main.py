e0dbfdd00bb344092319cc603069d8a81bc17dcc
utolee90 <itdatabasewiki@gmail.com>
2020-11-20 20:45:12 +0900

    Crawl 메인 오류 해결.

--- Crawl/main.py
+++ Crawl/main.py
@@ -27,13 +27,13 @@ with open('champion_id_and_name_list_kor.json', 'r', encoding='utf8') as g:
 #champ name to lowercase - without space.
 name_eng_simple = {}
 for key, val  in id_name_list.items():
-    if 'nunu' in val: #누누와 윌럼프 예외처리
+    if 'nunu' == val[0:4]: #누누와 윌럼프 예외처리
         name_eng_simple[key] = 'nunu'
     else:
         name_eng_simple[key] = val.lower().replace('\'', '').replace(' ', '').replace('_', '').replace('%20','').replace('.', '')
 name_kor_simple = {}
 for key, val in id_name_kor_list.items():
-    if '누누' in val:
+    if '누누'==val[0:2]:
         name_kor_simple[key] = '누누'
     else:
         name_kor_simple[key] = val.replace(' ', '').replace('_', '').replace('%20','').replace('.','')
@@ -41,9 +41,9 @@ for key, val in id_name_kor_list.items():
 def simplify(name): #단순화
     global id_name_list, id_name_kor_list, name_eng_simple, name_kor_simple
     name1 = ''
-    if 'nunu' in name: #누누와 윌럼프 예외처리
+    if 'nunu'== name[0:4]: #누누와 윌럼프 예외처리
         return 'nunu'
-    elif '누누' in name: 
+    elif '누누'== name[0:2]: #누누와 윌럼프 예외처리 
         return '누누'
     elif name[0] in [chr(i) for i in list(range(65,91))+list(range(97,123))]: #알파벳...
         name1= name.lower().replace('\'', '').replace(' ', '').replace('_', '').replace('%20','').replace('.','')
@@ -54,13 +54,12 @@ def simplify(name): #단순화
     else:
         return name
     
-
 def match_kor_eng(name, simple=False): #챔피언 이름 확인
     global id_name_list, id_name_kor_list, name_eng_simple, name_kor_simple
     key='0'
-    if 'nunu' in name:  #누누는 예외처리.
+    if 'nunu' == name[0:4]:  #누누는 예외처리.
         name = 'nunu'
-    elif '누누' in name:
+    elif '누누'== name[0:2]: #누누 예외처리
         name = '누누'
     else:
         name = name.lower().replace('\'', '').replace(' ', '').replace('_', '').replace('+','').replace('%20','').replace('.','')
@@ -86,6 +85,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
+    user_data_str = name
     #user data - 페이지 열 때에 생성
     if os.path.isfile('data.json'):
         with open('data.json', 'r', encoding='utf8') as f:
@@ -94,33 +94,50 @@ def user(name):
         user_data = {}
 
     global options
-    nowtime = float(time.time())
-    if not user_data.get(name) or nowtime - user_data[name]['time']>86400: #데이터가 없거나 하루 이상 지날 때 추가
+    if not user_data.get(name) or len(user_data.get(name))<=2: #데이터가 없을 때는 추가. 프리시즌이므로 지난시즌 정보는 갱신 X
         user_data_new = {}
         p_name = parse.quote_plus(name)
-        driver = webdriver.Chrome(options=options)
-        # 표현 언어를 영어로 바꾸고 싶으면 이 옵션 활성화시키기. 
-        # driver.get('https://op.gg/')
-        # driver.find_element_by_xpath('/html/body/div[2]/header/div[2]/div/div/div/div/div/button').click()
-        # time.sleep(2)
-        # lang_list = driver.find_elements_by_class_name('setting-list__item')
-        # for lang in lang_list:
-        #     if "English" in lang.text:
-        #         lang.click()
-        # time.sleep(2)
-        # driver.find_elements_by_class_name('setting__button')[0].click()
-        # time.sleep(2)
+        driver = webdriver.Chrome()
+        # #표현 언어를 영어로 바꾸고 싶으면 이 옵션 활성화시키기. 
+        driver.get('https://op.gg/')
+        driver.find_element_by_xpath('/html/body/div[2]/header/div[2]/div/div/div/div/div/button').send_keys(Keys.ENTER)
+        time.sleep(2)
+        lang_list = driver.find_elements_by_class_name('setting-list__item')
+        for lang in lang_list:
+            if "English" in lang.text:
+                lang.click()
+        time.sleep(2)
+        driver.find_elements_by_class_name('setting__button')[0].send_keys(Keys.ENTER)
+        time.sleep(2)
         # 옵션 끝
         driver.get(f'https://www.op.gg/summoner/champions/userName={p_name}')
-        driver.find_element_by_xpath('//*[@id="champion_season"]/li[2]/a').send_keys(Keys.ENTER)
-        trs = driver.find_elements_by_xpath("//*[@id=\"SummonerLayoutContent\"]/div[3]/div/div/div[2]/div[1]/table/tbody/tr")
-        
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="champion_season"]/li[2]/a').click()
+        time.sleep(5)
+        tables = driver.find_elements_by_tag_name('table') #테이블의 규칙으로 찾기
+        trs = tables[1].find_elements_by_xpath('./tbody/tr')
+        print('데이터 출력 길이', len(trs))
         for tr in trs:
+            #초기값 설정
+            win_cnt = '0W'; lose_cnt = '0L'
             try:
-                champ = tr.find_elements_by_xpath('./td[3]/a')[0].text
-                win_cnt = tr.find_elements_by_xpath('./td[4]/div/div/div[2]')[0].text
-                lose_cnt = tr.find_elements_by_xpath('./td[4]/div/div/div[4]')[0].text
-                user_data_new[champ] = [win_cnt, lose_cnt] # 리스트로 긁어오기. React 오류 방지
+                champ = simplify(tr.find_element_by_xpath('./td[3]/a').get_attribute('innerText')) #데이터 단순화...
+                print(champ)
+            except:
+                champ = ''
+            try:
+                cnt1 = tr.find_element_by_xpath('./td[4]/div/div/div[2]').get_attribute('innerText') #첫 번째
+                if cnt1[-1].upper() == 'W': win_cnt = cnt1 #W로 끝나면 win_cnt에 붙임.
+                elif cnt1[-1].upper() == 'L': lose_cnt = cnt1 #L로 끝나면 lose_cnt에 붙임.
+            except:
+                pass
+            try:
+                cnt2 = tr.find_element_by_xpath('./td[4]/div/div/div[4]').get_attribute('innerText') #두 번째
+                if cnt2[-1].upper() == 'L': lose_cnt = cnt2
+            except:
+                pass
+            try:
+                user_data_new[champ] = [win_cnt, lose_cnt] # 리스트로 처리. React 오류 방지
             except:
                 pass
         
@@ -134,7 +151,10 @@ def user(name):
         
         driver.quit()
     
-    user_data_str = json.dumps(user_data[name], ensure_ascii=False)
+    try:
+        user_data_str = json.dumps(user_data[name], ensure_ascii=False)
+    except:
+        user_data_str = ''
     
     return user_data_str
 
@@ -153,8 +173,6 @@ def champ(name):
         if name[0] in [chr(i) for i in range(44032, 55204)]:
             name = match_kor_eng(name, True).lower().replace(' ', '')  
         else: name = simplify(name) #영어 단순화
-    
-    print(name)
 
     if not champ_data.get(name) or nowtime - champ_data[name]['time']>86400: #데이터가 없거나 하루 이상 지날 때 추가
         champ_data_new = {}
@@ -189,12 +207,14 @@ def champ(name):
         
         driver.quit() #드라이버를 끄자..
         
-
-    champ_data_str = json.dumps(champ_data[name])
+    try:
+        champ_data_str = json.dumps(champ_data[name])
+    except:
+        champ_data_str = ''
     
     return champ_data_str
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=5000, debug=True)
     
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
-import {Tabs, Input, Button, Table } from 'antd';
+import {Tabs, Input, Button, Table, AutoComplete } from 'antd';
 import 'antd/dist/antd.css';
 import './Main.css';
 import BrowserRouter from 'react-router-dom';
@@ -12,20 +12,36 @@ const GetURL = 'http://localhost:5000/'
 
 function SearchBySummoner(){
 
-
-    const [username, setUsername] = React.useState('');
     const [result, setResult] = React.useState(Object()); //빈 오브젝트 생성
+    const [userList, setUserList] = React.useState([]); // 빈 리스트 생성
+    const [value, setValue] = React.useState('');
+    const [table_data, setTableData] = React.useState([]); //테이블 데이터로 생성
 
-    const changeName = (e) =>{ //이름 지정
-        setUsername(e.target.value);
-    }
+    const changeName = (data) => {
+        setValue(data);
+      };
 
     const submit_data = () =>{
-        Axios.get(GetURL+'user/'+username)
+        setTableData([{key:0, champion:'데이터 로딩중'}]);
+
+        Axios.get(GetURL+'user/'+value)
         .then(res =>{ 
             const { data } = res;
             console.log(data);
             setResult(data);
+            setUserList([...userList, {value: value}]) // 성공시에 userlist 추가
+            result['time']? //테이블 정보 갱신. 
+                setTableData(Object.entries(result).map((val) => { //var i=0; ++i;
+                if (val && val[0]!=='time') {
+                var win_rate_val = Math.round( parseInt(val[1][0])*10000/( parseInt(val[1][0])+parseInt(val[1][1])))/100;
+                var win_rate = win_rate_val+'%';
+                }
+                return (val && val[0]!== 'time'? 
+                {key:Object.keys(result).indexOf(val[0]) , champion:val[0], win:val[1][0], lost:val[1][1], 
+                 win_rate:win_rate}
+                : null);
+            })):
+            setTableData([])
         })
         .catch(error => console.log(error))
     }
@@ -39,7 +55,17 @@ function SearchBySummoner(){
         else if (array_kr_simple.indexOf(text) !==-1 ) {
             valx = array_en_simple[array_kr_simple.indexOf(text)] // en simple
             url = 'https://www.op.gg/champion/'+valx+'/statistics/'
-            return <a href={url}>{text}</a>;
+            return <a href={url}>{ChampData(text, 'kr', 'kr', false)}</a>; //원래 이름으로 출력
+            }
+        else if (array_en.indexOf(text) !==-1 ) {
+                valx = array_en_simple[array_en.indexOf(text)] // en simple
+                url = 'https://www.op.gg/champion/'+valx+'/statistics/'
+                return <a href={url}>{text}</a>;
+                }
+        else if (array_en_simple.indexOf(text) !==-1 ) {
+                    valx = text // en simple
+                    url = 'https://www.op.gg/champion/'+valx+'/statistics/'
+                    return <a href={url}>{ChampData(text, 'en', 'en', false)}</a>;
             }
         else {
             return text;
@@ -48,34 +74,19 @@ function SearchBySummoner(){
 
     const table_columns = [ 
         {title:'챔피언 이름', dataIndex:'champion', 
-        render: (text)=>(champ_name_link(text)) 
+        render: (text)=> (champ_name_link(text)) 
         }, 
         {title:'승리', dataIndex:'win'}, 
         {title:'패배', dataIndex:'lost'}, 
         {title:'승률', dataIndex:'win_rate'}
     ]
 
-    let table_data = []
-
-    if (result['time']) {
-
-    table_data = Object.entries(result).map((val) => {
-        if (val && val[0]!=='time') {
-        var win_rate_val = Math.round( parseInt(val[1][0])*10000/( parseInt(val[1][0])+parseInt(val[1][1])))/100;
-        var win_rate = win_rate_val+'%';
-        }
-        return (val && val[0]!== 'time'? 
-        {key:Object.entries(result).indexOf(val) , champion:val[0], win:val[1][0], lost:val[1][1], 
-         win_rate:win_rate}
-        : null);
-    })
-    }
     
     return (<div>
         <h2>소환사 이름으로 챔피언별 전적 검색</h2>
         <h3>참조 : <a href="https://op.gg/champion/"><LinkOutlined/> OP.GG</a> </h3>
         <div style={{'width':'480px'}}>
-        <Input placeholder="소환사명" name="username" onChange={changeName} style={{'width':'300px', 'float':'left'}}/>
+        <AutoComplete value={value} options={userList} placeholder="소환사명" name="username" onChange={changeName} style={{'width':'300px', 'float':'left'}}/>
         <Button type="button" onClick={submit_data} style={{'width':'96px'}}> 소환사 검색 </Button>
         </div>
         <br/>
@@ -93,53 +104,38 @@ function SearchByChampion(){
     const [champion, setChampion] = React.useState(''); 
     const [temp, setTemp] = React.useState(''); //갱신 오류를 방지하기 위해 챔피언 이름 임시저장 변수 생성
     const [result, setResult] = React.useState(Object()); //빈 오브젝트 생성
+    const [value, setValue] = React.useState('') // value
+    const [options, setOptions] = React.useState([]); // 자동완성 옵션...
+    const [table_data, setTableData] = React.useState([]);
+    const array_champ = [...array_kr, ...array_en]; 
 
-    const toTitleCase = (str) => {
-        return str.replace(
-          /\w\S*/g,
-          function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-          }
-        );
-      }
+    const changeName = (data) =>{ //이름 지정
+        setValue(data);
 
-    const changeName = (e) =>{ //이름 지정
-
-        if (array_id.indexOf(e.target.value) !==-1  ){ //숫자
-            setChampion(ChampData(e.target.value, 'num', 'en', true));
+        if (array_id.indexOf(data) !==-1  ){ //숫자
+            setChampion(ChampData(data, 'num', 'en', true));
         } 
-        
-        else if (array_en.indexOf(e.target.value) !==-1 || array_en_simple.indexOf(e.target.value) !==-1 ) { //영어
-            setChampion(ChampData(e.target.value, 'en', 'en', true));
-        }
-        else if (array_kr.indexOf(e.target.value) !==-1 || array_kr_simple.indexOf(e.target.value) !==-1 ){ //한글
-            setChampion(ChampData(e.target.value, 'kr', 'en', true));
+        else {
+            if (data.slice(0,4).toLowerCase() === 'nunu')
+            setValue(data.replace(' ','').replace("'", "").replace('.','').replace('_', '').toLowerCase()); // 데이터 단순화. 
+            if (array_en_simple.indexOf(data) !==-1 ) { //영어
+                setChampion(ChampData(data, 'en', 'en', true));
+            }
+            else if (array_kr_simple.indexOf(data) !==-1 ){ //한글
+                setChampion(ChampData(data, 'kr', 'en', true));
+            }
         }
         console.log(champion)
     }
 
-    const submit_data = () =>{
-        setTemp(champion); // 변수명을 챔피언 이름으로 일시지정.
-        Axios.get(GetURL+'champ/'+champion)
-        .then(res =>{ 
-            const { data } = res;
-            console.log(data);
-            setResult(data);
-            
-             
-        })
-        .catch(error => console.log(error))
+    const searchChamp = (text) => {
+        setOptions(!text? 
+            array_champ.map(v=>({value:v})) : 
+            array_champ.map(v=> (text==v.slice(0, text.length)?({value:v}):undefined)).filter(Boolean)
+        )
     }
 
-    const table_columns = [ 
-        {title:'포지션', dataIndex:'position'}, 
-        {title:'픽률', dataIndex:'pick_rate'}, 
-        {title:'승률', dataIndex:'win_rate'}
-    ];
-
-    let table_data = []
-
-    const null_drop = (arr) => {
+    const null_drop = (arr) => { //null 없애기...
         var res = [];
         if (typeof(arr)=='object'){
         for (var v of arr){
@@ -151,26 +147,46 @@ function SearchByChampion(){
         return res;
     }
 
-    if (result[(champion.toLowerCase())] && result['time']) {
-
-        table_data = null_drop(  Object.keys(result[(temp.toLowerCase())]).map(v => {
-            if (['Top', 'Mid', 'Jungler', 'Support','AD Carry'].indexOf(v)!==-1){
-                return (v?
-                    {key:Object.keys(result[(temp.toLowerCase())]).indexOf(v),
-                    position : v,
-                    pick_rate : result[(temp.toLowerCase())][v]['popularity'],
-                    win_rate : result[(temp.toLowerCase())][v]['win']
+    const submit_data = () =>{
+        setTemp(champion); // 변수명을 챔피언 이름으로 일시지정.
+        setTableData([{key:0, position:'Data Loading'}])
+        Axios.get(GetURL+'champ/'+champion)
+        .then(res =>{ 
+            const { data } = res;
+            console.log(data);
+            setResult(data);
+            (result[(champion.toLowerCase())] && result['time'])?
+            setTableData(null_drop(Object.keys(result[temp]).map(v => {
+                if (['Top', 'Mid', 'Jungler', 'Support','AD Carry'].indexOf(v)!==-1){
+                    return (v?
+                        {key:Object.keys(result[temp]).indexOf(v),
+                        position : v,
+                        pick_rate : result[temp][v]['popularity'],
+                        win_rate : result[temp][v]['win']
+                        }
+                       :null);
                     }
-                   :null);
                 }
-            }));
-        }
+                )))
+            :setTableData([]);
+            
+             
+        })
+        .catch(error => console.log(error))
+    }
+
+    const table_columns = [ 
+        {title:'포지션', dataIndex:'position'}, 
+        {title:'픽률', dataIndex:'pick_rate'}, 
+        {title:'승률', dataIndex:'win_rate'}
+    ];
+    
 
     return (<div>
         <h2>챔피언별 승률/전적</h2>
         <h3>참조 : <a href="https://leagueofgraphs.com/champions"><LinkOutlined/> League of Graphs</a> </h3>
         <div style={{'width':'480px'}}>
-        <Input placeholder="챔피언명(한글/영어/챔피언ID)" name="champion" onChange={changeName} style={{'width':'300px', 'float':'left'}}/>
+        <AutoComplete value={value} options={options} placeholder="챔피언명(한글/영어/챔피언ID)" name="champion" onSearch={searchChamp} onChange={changeName} style={{'width':'300px', 'float':'left'}}/>
         <Button type="button" onClick={submit_data} style={{'width':'96px'}}>챔피언 검색 </Button>
         </div>
         <br/>
@@ -189,6 +205,128 @@ function SearchByChampion(){
     </div>);
 }
 
+function TeamChampWin() {
+    const [result, setResult] = React.useState(Object()); //빈 오브젝트 생성
+    const [champion, setChampion] = React.useState('');  // 챔피언 이름 지정.
+    const [winrate, setWinRate] = React.useState(Array(5)) // 승률  
+    const [user0, setUser0] = React.useState(''); //사용자 이름
+    const [user1, setUser1] = React.useState(''); //사용자 이름
+    const [user2, setUser2] = React.useState(''); //사용자 이름
+    const [user3, setUser3] = React.useState(''); //사용자 이름
+    const [user4, setUser4] = React.useState(''); //사용자 이름
+    const [champ0, setChamp0] = React.useState(''); //챔피언 이름
+    const [champ1, setChamp1] = React.useState(''); //챔피언 이름
+    const [champ2, setChamp2] = React.useState(''); //챔피언 이름
+    const [champ3, setChamp3] = React.useState(''); //챔피언 이름
+    const [champ4, setChamp4] = React.useState(''); //챔피언 이름
+    
+    //사용자 이름 변경 함수
+    const changeName0 = (data) => (setUser0(data.target.value));
+    const changeName1 = (data) => (setUser1(data.target.value));
+    const changeName2 = (data) => (setUser2(data.target.value));
+    const changeName3 = (data) => (setUser3(data.target.value));
+    const changeName4 = (data) => (setUser4(data.target.value));
+   
+    // 챔피언 이름 변경함수
+    const changeChamp0 = (data) => (setChamp0(data.target.value));
+    const changeChamp1 = (data) => (setChamp1(data.target.value));
+    const changeChamp2 = (data) => (setChamp2(data.target.value));
+    const changeChamp3 = (data) => (setChamp3(data.target.value));
+    const changeChamp4 = (data) => (setChamp4(data.target.value));
+
+    const setNameEng = (data) => { // 영어이름으로 고치기... 
+        if (array_id.indexOf(data) !==-1  ){ //숫자
+            return ChampData(data, 'num', 'en', false);
+        }
+        else {
+            data = data.replace(' ','').replace("'", "").replace('.','').replace('_', '').toLowerCase(); // 문자 단순화
+            if (data.slice(0,2) === '누누') {
+                data='누누'; //데이터 단순화
+            }
+            else if (data.slice(0,4) === 'nunu') {
+                data = 'nunu';
+            }
+            if (array_en_simple.indexOf(data) !==-1 ) { //영어
+                return ChampData(data, 'en', 'en', false);
+            }
+            else if (array_kr_simple.indexOf(data) !==-1 ){ //한글
+                return ChampData(data, 'kr', 'en', false);
+            }
+            else {
+                return data;
+            }
+        }
+    };
+
+    let user = [user0, user1, user2, user3, user4]
+    let champ = [champ0, champ1, champ2, champ3, champ4]
+
+    const send_data = () =>{
+        if (user.filter(Boolean).length + champ.filter(Boolean).length ===10) { //모두 null이 아닐 때에만 데이터 전송.
+        setWinRate(['자료없음', '자료없음', '자료없음', '자료없음', '자료없음']);
+        for (var i=0; i<5;  i++){
+            var cname = setNameEng(champ[i]);
+            Axios.get(GetURL+'user/'+user[i])
+            .then(res =>{ 
+            const { data } = res;
+            console.log(data);
+            setResult(data);
+            if (result['time'] && result[cname]) { //승률 정보 갱신.
+                var win_rate_dat = Math.round( parseInt(result[cname][0])*10000/( parseInt(result[cname][0])+parseInt(result[cname][1])))/100 + '%'; 
+                setWinRate([0,1,2,3,4].map(v=> (i===v?win_rate_dat:winrate[v])));
+            }
+            else if (result['time'] && !result[cname]) {
+                setWinRate([0,1,2,3,4].map(v=> (i===v?'전적 없음':winrate[v])));
+            }
+        })
+        .catch(error => {console.log(error);
+            setWinRate([0,1,2,3,4].map(v=> (i===v?'조회 실패':winrate[v])));
+        })
+
+
+        }
+    }
+
+    }
+
+
+    return(<div> <h2>팀원들의 각 챔피언 승률 및 전적</h2>
+    <h3>참조 : <a href="https://op.gg/champion/"><LinkOutlined/> OP.GG</a> </h3>
+    <div>사용자 ID와 챔피언 정보를 전부 입력해야 데이터를 조회할 수 있습니다.</div>
+        <div style={{'width':'480px'}}>
+                <div key={0}>
+                    <Input addonBefore={'유저1'} placeholder={'유저명1'} style={{'width':'220px'}} onChange={changeName0} value={user0}/>
+                    <Input placeholder={'챔피언1'} style={{'width':'160px'}} onChange={changeChamp0} value={champ0}/>
+                    <div>전적: {winrate[0]}</div>
+                </div>
+                <div key={1}>
+                    <Input addonBefore={'유저2'} placeholder={'유저명2'} style={{'width':'220px'}} onChange={changeName1} value={user1}/>
+                    <Input placeholder={'챔피언2'} style={{'width':'160px'}} onChange={changeChamp1} value={champ1}/>
+                    <div>전적 : {winrate[1]}</div>
+                </div>
+                <div key={2}>
+                    <Input addonBefore={'유저3'} placeholder={'유저명3'} style={{'width':'220px'}} onChange={changeName2} value={user2}/>
+                    <Input placeholder={'챔피언3'} style={{'width':'160px'}} onChange={changeChamp2} value={champ2}/>
+                    <div>전적 : {winrate[2]}</div>
+                </div>
+                <div key={3}>
+                    <Input addonBefore={'유저4'} placeholder={'유저명4'} style={{'width':'220px'}} onChange={changeName3} value={user3}/>
+                    <Input placeholder={'챔피언4'} style={{'width':'160px'}} onChange={changeChamp3} value={champ3}/>
+                    <div>전적 : {winrate[3]}</div>
+                </div>
+                <div key={4}>
+                    <Input addonBefore={'유저5'} placeholder={'유저명5'} style={{'width':'220px'}} onChange={changeName4} value={user4}/>
+                    <Input placeholder={'챔피언5'} style={{'width':'160px'}} onChange={changeChamp4} value={champ4}/>
+                    <div>전적 : {winrate[4]}</div>
+                </div>
+            
+            <Button onClick={send_data}>승/패 검색</Button>
+        </div>
+    </div>
+    )
+
+}
+
 function Main() {
     return(
     <Tabs className="main-content" defaultActiveKey="1" style={{'width':'480px', 'padding':'10px'}}>
@@ -197,6 +335,9 @@ function Main() {
         </TabPane>
         <TabPane tab="챔피언별 승률" key="2">
             <SearchByChampion/>
+        </TabPane>
+        <TabPane tab="팀 챔피언 승률" key="3">
+            <TeamChampWin/>
         </TabPane>
     </Tabs>
     );
